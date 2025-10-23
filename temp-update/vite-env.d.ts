16bf14e3332562eac65c17f69c1b09b78aff6545
lllogggs <159713119+lllogggs@users.noreply.github.com>
2025-10-23 23:54:39 +0900

    Delete temp-update directory

--- temp-update/vite-env.d.ts
+++
@@ -1,3 +0,0 @@
-// FIX: Commented out to resolve "Cannot find type definition file for 'vite/client'" error.
-// The types are not used in the project's client-side code.
-// /// <reference types="vite/client" />
